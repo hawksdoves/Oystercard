377d12868b3d4278c012f5102c75089540f5adab
phoebe <phoebe.greig@gmail.com>
2016-03-08 17:03:25 +0000

    rspec work

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -10,23 +10,28 @@ class JourneyLog
 
 	def start entry_station
 		journey = Journey.new
-	  journey.begin(entry_station)
-	   @history << journey
+	  	journey.begin(entry_station)
+	   	@history << journey
 	end
 
 	def finish exit_station
-		current_journey
-		my_key = @history[-1].a_journey.keys[0]
-  	@history[-1].a_journey[my_key] = exit_station
+		current_journey = journey_status
+		if current_journey.a_journey.empty? 
+			current_journey.end(exit_station)
+			@history << current_journey
+		else	
+			my_key = @history[-1].a_journey.keys[0]
+	  		@history[-1].a_journey[my_key] = exit_station
+  		end
 	end
 
 	def journeys
-		@history
+		@history.map! { |journey| journey.a_journey }
 	end
 
+private
 
-
-	def current_journey
+	def journey_status
 		@history[-1].incomplete? ?  @history[-1] : Journey.new
 	end
 
