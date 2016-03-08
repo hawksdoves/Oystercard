9c77bb28d102e5ae23784ffc9cda0813eebf1326
Chris Parker <cjp214@gmail.com>
2016-03-08 10:15:25 +0000

    added test for deducting balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,14 +12,20 @@ describe Oystercard do
 
   describe '#top_up' do
     it 'increases balance by argument when you top up' do
-      expect{ oystercard.top_up 25 }.to change{ oystercard.balance }.by 25
+      expect{ oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
     end
 
     it 'has a maximum balance of £90' do
       error = Oystercard::MAX_BALANCE_ERROR
-      expect{ oystercard.top_up 91 }.to raise_error error
+      expect{oystercard.top_up(Oystercard::MAX_BALANCE + 1)}.to raise_error error
     end
 
+  end
+
+  describe '#deduct' do
+    it 'deducts fare cost from the card balance' do
+      expect{ oystercard.deduct 1}.to change{oystercard.balance}.by -1
+    end
   end
 
 end
